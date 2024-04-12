7f30a104b1b009b854971c3ad1563648b5157979
Edmund Miller <git@edmundmiller.dev>
2024-04-12 13:01:44 -0500

    refactor: Clean up Galleries

---
+++ src/content/galleries/index.js
@@ -0,0 +1,28 @@
+const galleries = [
+  {
+    title: 'Rice',
+    date: '2017-11-18',
+    link: 'https://www.danieldphotos.com/UTD-Rugby-2016/UTD-Rugby-111817-Rice',
+    key: 'Vfb54C',
+  },
+  {
+    title: 'LSC 7s State Championship',
+    date: '2017-04-22',
+    link: 'https://www.danieldphotos.com/UTD-Rugby-2016/LSC-7s-ST-Champ-042217',
+    key: 'tPgthW',
+  },
+  {
+    title: 'St. Edwards',
+    date: '2016-02-20',
+    link: 'https://www.danieldphotos.com/UTD-Rugby-2016/UTD-Rugby-022016',
+    key: 'D8kpPc',
+  },
+  {
+    title: 'University of Dallas',
+    date: '2016-01-23',
+    link: 'https://www.danieldphotos.com/UTD-Rugby-2016/UTD-Rugby-012316',
+    key: 'FDkSMV',
+  },
+];
+
+export default galleries;
